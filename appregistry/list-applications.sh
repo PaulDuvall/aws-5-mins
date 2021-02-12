6e66f8698e14819eaf8417fd4933d52b4312e54d
Paul Duvall <pduvall@gmail.com>
2021-02-12 21:31:14 +0000

    changes

--- appregistry/list-applications.sh
+++ appregistry/list-applications.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+searchGroupName=Commerce_Payments
+listApps=$(aws servicecatalog-appregistry list-applications --query 'applications[*].name' --output text --region us-east-1)
+
+for app in ${listApps[*]}
+  do
+    attrGroup=$(aws servicecatalog-appregistry list-associated-attribute-groups --application $app --max-results 1 --query 'attributeGroups[0]' --output text --region us-east-1)
+    
+    groupName=$(aws servicecatalog-appregistry get-attribute-group --attribute-group $attrGroup --query 'attributes' --output text --region us-east-1)
+    if [ $groupName = $searchGroupName ]; then printf "$app\n"; fi
+  done
