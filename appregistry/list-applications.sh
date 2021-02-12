5219d291eb95310bd9d950c999b413db452c2faf
Paul Duvall <pduvall@gmail.com>
2021-02-12 21:38:29 +0000

    changes

--- appregistry/list-applications.sh
+++ appregistry/list-applications.sh
@@ -6,7 +6,6 @@ listApps=$(aws servicecatalog-appregistry list-applications --query 'application
 for app in ${listApps[*]}
   do
     attrGroup=$(aws servicecatalog-appregistry list-associated-attribute-groups --application $app --max-results 1 --query 'attributeGroups[0]' --output text --region us-east-1)
-    
-    groupName=$(aws servicecatalog-appregistry get-attribute-group --attribute-group $attrGroup --query 'attributes' --output text --region us-east-1)
+    groupName=$(aws servicecatalog-appregistry get-attribute-group --attribute-group $attrGroup --query 'name' --output text --region us-east-1)
     if [ $groupName = $searchGroupName ]; then printf "$app\n"; fi
   done
