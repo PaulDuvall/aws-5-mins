1d5e2116bb7be6452830bb0547a66900d25c1e61
Paul Duvall <pduvall@gmail.com>
2021-02-12 21:27:13 +0000

    changes

--- appregistry/list-attrib-groups.sh
+++ appregistry/list-attrib-groups.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-listApps=$(aws servicecatalog-appregistry list-applications --query 'applications[*].name' --output text)
+listApps=$(aws servicecatalog-appregistry list-applications --query 'applications[*].name' --output text --region us-east-1)
 
 for app in ${listApps[*]}
   do
-    attrGroup=$(aws servicecatalog-appregistry list-associated-attribute-groups --application $app --max-results 1 --query 'attributeGroups[0]' --output text)
+    attrGroup=$(aws servicecatalog-appregistry list-associated-attribute-groups --application $app --max-results 1 --query 'attributeGroups[0]' --output text --region us-east-1)
     printf "Attributes for $app\n"
-    aws servicecatalog-appregistry get-attribute-group --attribute-group $attrGroup --query 'attributes'
+    aws servicecatalog-appregistry get-attribute-group --attribute-group $attrGroup --query 'attributes' --region us-east-1
   done
